8863d1385a9ba524975ca2aadfb32ebcc2799648
Itay <59503091+momohakarish@users.noreply.github.com>
2020-09-19 23:40:21 +0300

    Add files via upload

--- Leetcode/728. Self Dividing Numbers.py
+++ Leetcode/728. Self Dividing Numbers.py
@@ -3,6 +3,8 @@ from typing import List
 
 """
 Checks if the number is self dividing according to the definition given by the question
+Time complexity is O(l) with l being the number of digits in the number
+Space complexity is O(1)
 """
 
 
@@ -15,6 +17,9 @@ def self_dividing(num: int) -> bool:
 
 """
 The idea is simple and it's just iterating over all the numbers in the range given and checking if they are self dividing
+Using the schema of [L ,R] to represent our range
+Time complexity is O(n * l) with n being (R - L)
+Space complexity is O(n)
 """
 
 
