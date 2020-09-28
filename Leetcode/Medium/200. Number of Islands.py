6b6b7c8305479b722ad8daff0e7102611e777d93
Itay <59503091+momohakarish@users.noreply.github.com>
2020-09-29 00:14:06 +0300

    Add files via upload

--- Leetcode/Medium/200. Number of Islands.py
+++ Leetcode/Medium/200. Number of Islands.py
@@ -6,23 +6,23 @@ We increment a counter each time we encounter a 1 and simply return it at the en
 """
 
 
-def delete(self, grid: List[List[str]], i, j):
+def delete(grid: List[List[str]], i: int, j: int):
     if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]):
         return
     if grid[i][j] == '0':
         return
     grid[i][j] = '0'
-    self.delete(grid, i + 1, j)
-    self.delete(grid, i - 1, j)
-    self.delete(grid, i, j + 1)
-    self.delete(grid, i, j - 1)
+    delete(grid, i + 1, j)
+    delete(grid, i - 1, j)
+    delete(grid, i, j + 1)
+    delete(grid, i, j - 1)
 
 
-def numIslands(self, grid: List[List[str]]) -> int:
+def numIslands(grid: List[List[str]]) -> int:
     count = 0
     for i, row in enumerate(grid):
         for j, cell in enumerate(row):
             if grid[i][j] == '1':
                 count += 1
-                self.delete(grid, i, j)
+                delete(grid, i, j)
     return count
