94a3ad4645a892f8eb744c528297cb96dca58e80
Itay <59503091+momohakarish@users.noreply.github.com>
2021-07-15 05:43:29 +0300

    Update 1491. Average Salary Excluding the Minimum and Maximum Salary.py

--- Leetcode/Easy/1491. Average Salary Excluding the Minimum and Maximum Salary.py
+++ Leetcode/Easy/1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -2,7 +2,7 @@ from typing import List
 
 """
 The idea is simple just find the max, min, and sum of the array and then calculate the result using this formula:
-result = (sum - max - min) - (len(salary) - 2)
+result = (sum - max - min) / (len(salary) - 2)
 
 Time complexity is O(n) with n being the length of the array
 Space complexity is O(1) 
